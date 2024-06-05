9671d2d48dfa3319cbd11ac0a7855d160cf52fc4
willgpennell <wpennell48@gmail.com>
2024-06-05 15:08:36 -0230

    code addition added!

--- main.py
+++ main.py
@@ -10,3 +10,18 @@ sl.markdown('---')
 sl.markdown('[Google](https://www.google.com)')
 sl.markdown('---')
 sl.caption('Hi I am caption')
+
+# latex works in this software aswell
+sl.latex(r'\begin{pmatrix}a&b\\c&d\end{pmatrix}')
+json = {"a":"1,2,3",
+        "b":"4,5,6"}
+sl.json(json)
+
+code=""" 
+print("Hello World!!")
+    def function():
+        return 0;
+"""
+
+sl.code(code, language="python")
+
